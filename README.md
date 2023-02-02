90ba2f1437d2b0993ed50b30ff960ef5746e6eed
ggallo22 <92111625+ggallo22@users.noreply.github.com>
2023-02-01 16:30:06 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Test in different browsers see it scan through
+
+First, run the development server:
+
+```
+npm run test:e2e
+```
+```
+npx playwright test home.spec.ts --headed
+
+```
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+
+
+export default function Home() {
+  return (
+    <main className={styles.container} id="colouring">
+      <nav>
+        <Link href="/about">About</Link>
+      </nav>
+    </main>
+  )
+}
